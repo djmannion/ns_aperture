f952c849ae4539a1216308b1f8097028e2d08c7c
Damien J. Mannion (BLiSS) <dmannion@umn.edu>
2012-09-24 01:00:51 -0500

    -

--- fmri_analysis/mvpa.py
+++ fmri_analysis/mvpa.py
@@ -6,6 +6,7 @@ apertures fMRI experiment.
 from __future__ import division
 
 import os, os.path
+import itertools
 
 import numpy as np
 import scipy.stats
@@ -70,6 +71,11 @@ def data_xtr( paths, conf ):
 
 	os.chdir( paths[ "mvpa" ][ "base_dir" ] )
 
+	# we use this in checks
+	n_vols_per_run = int( conf[ "exp" ][ "run_len_s" ] /
+	                      conf[ "acq" ][ "tr_s" ]
+	                    )
+
 	for ( roi_name, _ ) in conf[ "ana" ][ "rois" ]:
 
 		data = []
@@ -77,6 +83,7 @@ def data_xtr( paths, conf ):
 
 		for ( i_hemi, hemi ) in enumerate( [ "lh", "rh" ] ):
 
+			# the mask file contains the parcel value for each node in the ROI
 			mask_file = "%s_%s_%s-full.niml.dset" % ( paths[ "loc" ][ "roi_parc" ],
 			                                          roi_name,
 			                                          hemi
@@ -86,9 +93,10 @@ def data_xtr( paths, conf ):
 
 			for ( i_run, filt_file ) in enumerate( paths[ "mvpa" ][ "filt_files" ] ):
 
-				# our input dataset
+				# input data is the (full) filtered timeseries for this run
 				data_file = "%s_%s-full.niml.dset" % ( filt_file, hemi )
 
+				# we only need this file temporarily
 				out_file = os.path.join( paths[ "mvpa" ][ "base_dir" ], "temp.txt" )
 
 				if os.path.exists( out_file ):
@@ -109,37 +117,68 @@ def data_xtr( paths, conf ):
 				                          log_path = paths[ "summ" ][ "log_file" ]
 				                        )
 
+				# the resulting data should be ( nodes, time + node index + parcel
+				# value )
 				curr_data = np.loadtxt( out_file )
+
+				# check this claim
+				assert( curr_data.shape[ 1 ] == ( n_vols_per_run + 2 ) )
 
 				# clean up the unwanted output
 				if os.path.exists( out_file ):
 					os.remove( out_file )
 
+				# append the timeseries data (ie. not including the node index or the
+				# parcel value)
 				run_data.append( curr_data[ :, 1:-1 ] )
 
+				# we also want to know the node info, but since this is the same for
+				# all runs we need only compute it once
 				if i_run == 0:
+					# we'd like to know which hemisphere it came from
 					hemi_info = np.ones( ( curr_data.shape[ 0 ], 1 ) ) * i_hemi
+					# and the node index and parcel value
 					node_info.append( np.hstack( ( curr_data[ :, [ 0, -1 ] ], hemi_info ) ) )
 
+			# run_data is a 10-item (runs) list of ( nodes x time ) arrays
+			# converting it to array makes it ( run x nodes x time ) array
 			run_data = np.array( run_data )
 
-			data.append( run_data )
+			# check shape is as expected
+			assert( run_data.shape[ 0 ] == conf[ "subj" ][ "n_exp_runs" ] )
+			assert( run_data.shape[ 2 ] == n_vols_per_run )
 
-		# data is runs x nodes x time
+			data.append( np.array( run_data ) )
+
+		# data is a 2-item (hemisphere) list of ( runs x nodes x time) arrays
+		# want to join data along the nodes dimension; pass this axis (1) to
+		# concatenate (which doesnt count the list dimension)
+		# data becomes runs x nodes x time
 		data = np.concatenate( data, axis = 1 )
-		# node_info is nodes x 3 ( node index, parcel index, hemi )
+
+		assert( data.shape[ 0 ] == conf[ "subj" ][ "n_exp_runs" ] )
+		assert( data.shape[ 2 ] == n_vols_per_run )
+
+		# node_info is a 2-item (hemisphere) list of nodes x 3 arrays
+		# node_info becomes nodes (lh then rh) x 3 ( node index, parcel index,
+		# hemi )
 		node_info = np.vstack( node_info )
 
+		# a region type (stimulated or 'blank') is defined by its parcel(s)
 		for ( i_parc, parc_ind ) in enumerate( conf[ "ana" ][ "i_parc" ] ):
 
+			# find the indices of nodes that correspond to any of the parcel indices
+			# in this region type
 			i_parc_nodes = np.any( [ node_info[ :, 1 ] == k
 			                         for k in parc_ind
 			                       ], axis = 0
 			                     )
 
+			# pull out the data and info from these nodes
 			parc_data = data[ :, i_parc_nodes, : ]
 			parc_info = node_info[ i_parc_nodes, : ]
 
+			# ... and save
 			parc_data_file = "%s_%s_%s.npy" % ( paths[ "mvpa" ][ "parc_data" ],
 			                                    roi_name,
 			                                    conf[ "ana" ][ "parc_lbl" ][ i_parc ]
@@ -204,9 +243,12 @@ def info( paths, conf ):
 
 					time_vol = time_s / conf[ "acq" ][ "tr_s" ]
 
+					# correct for the HRF delay
 					time_adj = time_vol + conf[ "ana" ][ "hrf_corr_vol" ]
 
 					run_info[ i_run, i_block, 0 ] = time_adj
+
+	assert( np.sum( np.isnan( run_info ) ) == 0 )
 
 	np.save( paths[ "mvpa" ][ "blk_data_info" ], run_info )
 
@@ -214,76 +256,81 @@ def info( paths, conf ):
 def blk_data_xtr( paths, conf ):
 	"""Extracts blocked and z-scored data"""
 
-	n_vol_per_block = conf[ "exp" ][ "block_len_s" ] / conf[ "acq" ][ "tr_s" ]
+	n_vol_per_block = int( conf[ "exp" ][ "block_len_s" ] /
+	                       conf[ "acq" ][ "tr_s" ]
+	                     )
 
+	# blk_data_info is runs x blocks x details ( 0 = onset, 1 = cond )
 	blk_data_info = np.load( paths[ "mvpa" ][ "blk_data_info" ] )
 
-	for ( roi_name, _ ) in conf[ "ana" ][ "rois" ]:
+	for ( ( roi_name, _ ), parc_lbl ) in itertools.product( conf[ "ana" ][ "rois" ],
+	                                                        conf[ "ana" ][ "parc_lbl" ]
+	                                                      ):
 
-		for parc_lbl in conf[ "ana" ][ "parc_lbl" ]:
+		parc_data_file = "%s_%s_%s.npy" % ( paths[ "mvpa" ][ "parc_data" ],
+		                                    roi_name,
+		                                    parc_lbl
+		                                  )
 
-			parc_data_file = "%s_%s_%s.npy" % ( paths[ "mvpa" ][ "parc_data" ],
-			                                    roi_name,
-			                                    parc_lbl
-			                                  )
+		# parc_data is runs x nodes x time
+		parc_data = np.load( parc_data_file )
 
-			# parc_data is runs x nodes x time
-			parc_data = np.load( parc_data_file )
+		( n_runs, n_nodes, n_vols ) = parc_data.shape
+		( _, n_blocks, _ ) = blk_data_info.shape
 
-			( n_runs, n_nodes, n_vols ) = parc_data.shape
-			( _, n_blocks, _ ) = blk_data_info.shape
+		blk_data = np.empty( ( n_runs, n_blocks, n_nodes ) )
+		blk_data.fill( np.NAN )
 
-			blk_data = np.empty( ( n_runs, n_blocks, n_nodes ) )
-			blk_data.fill( np.NAN )
+		for i_run in xrange( n_runs ):
+			for i_block in xrange( n_blocks ):
 
-			for i_run in xrange( n_runs ):
+				# onset volume for this block, HRF corrected
+				i_block_start = blk_data_info[ i_run, i_block, 0 ]
 
-				for i_block in xrange( n_blocks ):
+				# get the volume range for this block
+				i_vol_range = np.arange( i_block_start,
+				                         i_block_start + n_vol_per_block,
+				                       ).astype( "int" )
 
-					# onset volume for this block, HRF corrected
-					i_block_start = blk_data_info[ i_run, i_block, 0 ]
+				# blk_tc becomes nodes x time
+				blk_tc = parc_data[ i_run, ... ][ :, i_vol_range ]
 
-					# get the volume range for this block
-					i_vol_range = np.arange( i_block_start,
-					                         i_block_start + n_vol_per_block,
-					                       ).astype( "int" )
+				# just verify that is indeed the case
+				assert( blk_tc.shape == ( n_nodes, len( i_vol_range ) ) )
 
-					# this extraction flips the dimensions around; `blk_tc` is vol x node,
-					# surprisingly
-					blk_tc = parc_data[ i_run, :, i_vol_range ]
+				# ... because we want to average over the vol dimension
+				blk_mean = np.mean( blk_tc, axis = 1 )
 
-					# just verify that is indeed the case
-					assert( blk_tc.shape == ( len( i_vol_range ), n_nodes ) )
+				# average over block timepoints
+				blk_data[ i_run, i_block, : ] = blk_mean
 
-					# ... because we want to average over the vol dimension
-					blk_mean = np.mean( blk_tc, axis = 0 )
+			# z-score all the blocks in this run, for each node
+			blk_data[ i_run, :, : ] = scipy.stats.zscore( blk_data[ i_run, :, : ],
+			                                              axis = 0
+			                                            )
 
-					# average over block timepoints
-					blk_data[ i_run, i_block, : ] = blk_mean
+		assert( np.sum( np.isnan( blk_data ) ) == 0 )
 
-				# z-score all the blocks in this run, for each node
-				blk_data[ i_run, :, : ] = scipy.stats.zscore( blk_data[ i_run, :, : ],
-				                                              axis = 0
-				                                            )
+		blk_file = "%s_%s_%s.npy" % ( paths[ "mvpa" ][ "blk_data" ],
+		                              roi_name,
+		                              parc_lbl
+		                            )
 
-			blk_file = "%s_%s_%s.npy" % ( paths[ "mvpa" ][ "blk_data" ],
-			                              roi_name,
-			                              parc_lbl
-			                            )
-
-			np.save( blk_file, blk_data )
+		np.save( blk_file, blk_data )
 
 
 def rfe_classify( paths, conf ):
 	"""Runs recursive feature elimination classification"""
 
 	# load the info for each block, which doesn't depend on hemisphere
+	# blk_data_info is runs x blocks x details ( 0 = onset, 1 = cond )
 	blk_data_info = np.load( paths[ "mvpa" ][ "blk_data_info" ] )
 
 	svm_temp_dir = os.path.join( paths[ "mvpa" ][ "base_dir" ], "svm_temp" )
 
 	log_path = paths[ "mvpa" ][ "log_file" ]
 
+	# run *indices* providing the training data for each fold
 	train_info = [ [ 2, 3, 4, 5, 6, 7, 8, 9 ],
 	               [ 0, 1, 4, 5, 6, 7, 8, 9 ],
 	               [ 0, 1, 2, 3, 6, 7, 8, 9 ],
@@ -291,6 +338,7 @@ def rfe_classify( paths, conf ):
 	               [ 0, 1, 2, 3, 4, 5, 6, 7 ]
 	             ]
 
+	# ... and test data
 	test_info = [ [ 0, 1 ],
 	              [ 2, 3 ],
 	              [ 4, 5 ],
@@ -298,107 +346,159 @@ def rfe_classify( paths, conf ):
 	              [ 8, 9 ]
 	            ]
 
-	for ( roi_name, _ ) in conf[ "ana" ][ "rois" ]:
+	for ( ( roi_name, _ ), parc_lbl ) in itertools.product( conf[ "ana" ][ "rois" ],
+	                                                        conf[ "ana" ][ "parc_lbl" ]
+	                                                      ):
 
-		for parc_lbl in conf[ "ana" ][ "parc_lbl" ]:
+		roi_parc_dir = os.path.join( paths[ "mvpa" ][ "rfe_base_dir" ],
+		                             "%s_%s" % ( roi_name, parc_lbl )
+		                           )
 
-			roi_parc_dir = os.path.join( paths[ "mvpa" ][ "rfe_base_dir" ],
-			                             "%s_%s" % ( roi_name, parc_lbl )
-			                           )
+		blk_data_file = "%s_%s_%s.npy" % ( paths[ "mvpa" ][ "blk_data" ],
+		                                   roi_name,
+		                                   parc_lbl
+		                                 )
 
-			blk_data_file = "%s_%s_%s.npy" % ( paths[ "mvpa" ][ "blk_data" ],
-			                                   roi_name,
-			                                   parc_lbl
-			                                 )
+		# blk_data is runs x blocks x nodes
+		blk_data = np.load( blk_data_file )
 
-			blk_data = np.load( blk_data_file )
+		( n_runs, n_blocks, n_nodes ) = blk_data.shape
 
-			( n_runs, n_blocks, n_nodes ) = blk_data.shape
+		parc_info_file = "%s_%s_%s.npy" % ( paths[ "mvpa" ][ "parc_info" ],
+		                                    roi_name,
+		                                    parc_lbl
+		                                  )
 
-			svm_runs = zip( train_info, test_info )
+		# parc_info is nodes x 3 ( node index, parcel value, hemi )
+		parc_info = np.load( parc_info_file )
 
-			n_folds = len( svm_runs )
+		# this holds the indices for all the nodes
+		i_nodes = parc_info[ :, 0 ]
+		i_hemis = parc_info[ :, 2 ]
 
-			acc = np.empty( ( n_folds, conf[ "ana" ][ "rfe_levels" ] ) )
+		assert( parc_info.shape[ 0 ] == n_nodes )
 
-			for ( i_fold, ( train_runs, test_runs ) ) in enumerate( svm_runs ):
+		svm_runs = zip( train_info, test_info )
 
-				fold_dir = os.path.join( roi_parc_dir, "fold%02d" % ( i_fold + 1 ) )
+		n_folds = len( svm_runs )
 
-				# check that there is no contamination of train and test sets
-				assert( set.isdisjoint( set( train_runs ), set( test_runs ) ) )
+		# this will store our accuracy for each fold and RFE level
+		acc = np.empty( ( n_folds, conf[ "ana" ][ "rfe_levels" ] ) )
+		acc.fill( np.NAN )
 
-				train_data = blk_data[ train_runs, :, : ]
-				train_data_info = blk_data_info[ train_runs, :, 1 ]
+		for ( i_fold, ( train_runs, test_runs ) ) in enumerate( svm_runs ):
 
-				( n_train_runs, n_train_blocks, n_train_nodes ) = train_data.shape
+			fold_dir = os.path.join( roi_parc_dir, "fold%02d" % ( i_fold + 1 ) )
 
-				n_splits = int( n_train_runs / 2 )
+			# check that there is no contamination of train and test sets
+			assert( set.isdisjoint( set( train_runs ), set( test_runs ) ) )
 
-				test_data = blk_data[ test_runs, :, : ]
-				test_data_info = blk_data_info[ test_runs, :, 1 ]
+			train_data = blk_data[ train_runs, :, : ]
 
-				( n_test_runs, n_test_blocks, n_test_nodes ) = test_data.shape
+			assert( train_data.shape == ( len( train_runs ), n_blocks, n_nodes ) )
 
-				for i_level in xrange( conf[ "ana" ][ "rfe_levels" ] ):
+			train_data_info = blk_data_info[ train_runs, :, 1 ]
 
-					level_dir = os.path.join( fold_dir, "rfe%02d" % ( i_level + 1 ) )
+			assert( train_data_info.shape == ( len( train_runs ), n_blocks ) )
 
-					split_wgt = np.empty( ( n_train_nodes, n_splits ) )
-					split_wgt.fill( np.NAN )
+			( n_train_runs, n_train_blocks, n_train_nodes ) = train_data.shape
 
-					for i_split in xrange( n_splits ):
+			# make a copy of the node indices to use for this fold
+			i_fold_nodes = i_nodes
+			i_fold_hemis = i_hemis
 
-						split_dir = os.path.join( level_dir, "split%02d" % ( i_split + 1 ) )
+			# how many splits to do per RFE level
+			n_splits = int( n_train_runs / 2 )
 
-						# extract training data for this split
-						i_split_runs = np.delete( np.arange( n_train_runs ),
-						                          [ i_split * 2, i_split * 2 + 1 ]
-						                        )
+			test_data = blk_data[ test_runs, :, : ]
+			test_data_info = blk_data_info[ test_runs, :, 1 ]
 
-						split_data = train_data[ i_split_runs, :, : ]
-						split_data_info = train_data_info[ i_split_runs, : ]
+			( n_test_runs, n_test_blocks, n_test_nodes ) = test_data.shape
 
-						split_wgt[ :, i_split ] = _get_split_weights( split_dir,
-						                                              split_data,
-						                                              split_data_info,
-						                                              None  # log_path
-						                                            )
+			for i_level in xrange( conf[ "ana" ][ "rfe_levels" ] ):
 
-					# average weights over splits
-					wgt = np.mean( split_wgt, axis = 1 )
+				level_dir = os.path.join( fold_dir, "rfe%02d" % ( i_level + 1 ) )
 
-					# sort |weights| (in ascending order )
-					i_wgt = np.argsort( np.abs( wgt ) )
+				split_wgt = np.empty( ( n_train_nodes, n_splits ) )
+				split_wgt.fill( np.NAN )
 
-					# cull lowest performing weights
-					n_to_cull = np.round( n_train_nodes * conf[ "ana" ][ "rfe_cull_p" ] )
+				for i_split in xrange( n_splits ):
 
-					# update
-					train_data = train_data[ :, :, i_wgt[ n_to_cull: ] ]
-					n_train_nodes = train_data.shape[ -1 ]
+					split_dir = os.path.join( level_dir, "split%02d" % ( i_split + 1 ) )
 
-					test_data = test_data[ :, :, i_wgt[ n_to_cull: ] ]
-					n_test_nodes = test_data.shape[ -1 ]
+					# extract training data for this split
+					i_split_runs = np.delete( np.arange( n_train_runs ),
+					                          [ i_split * 2, i_split * 2 + 1 ]
+					                        )
 
-					[ lvl_acc, lvl_wgt ] = _run_level( level_dir,
-					                                   train_data,
-					                                   train_data_info,
-					                                   test_data,
-					                                   test_data_info,
-					                                   None  #log_path
-					                                 )
+					split_data = train_data[ i_split_runs, :, : ]
+					split_data_info = train_data_info[ i_split_runs, : ]
 
-					acc[ i_fold, i_level ] = lvl_acc
+					split_wgt[ :, i_split ] = _get_split_weights( split_dir,
+					                                              split_data,
+					                                              split_data_info,
+					                                              log_path
+					                                            )
 
-			acc_file = os.path.join( roi_parc_dir,
-			                         "%s_%s_%s.txt" % ( paths[ "mvpa" ][ "acc_base" ],
-			                                            roi_name,
-			                                            parc_lbl
-			                                          )
-			                       )
+				# average weights over splits
+				wgt = np.mean( split_wgt, axis = 1 )
 
-			np.savetxt( acc_file, acc )
+				# get the indices that would sort |weights| (in ascending order )
+				i_wgt = np.argsort( np.abs( wgt ) )
+
+				# cull lowest performing weights
+				n_to_cull = np.round( n_train_nodes * conf[ "ana" ][ "rfe_cull_p" ] )
+
+				# update
+				train_data = train_data[ :, :, i_wgt[ n_to_cull: ] ]
+				n_train_nodes = train_data.shape[ -1 ]
+
+				test_data = test_data[ :, :, i_wgt[ n_to_cull: ] ]
+				n_test_nodes = test_data.shape[ -1 ]
+
+				# cull from the list of node indices
+				i_fold_nodes = i_fold_nodes[ i_wgt[ n_to_cull: ] ]
+				assert( len( i_fold_nodes ) == n_train_nodes )
+
+				i_fold_hemis = i_fold_hemis[ i_wgt[ n_to_cull: ] ]
+				assert( len( i_fold_hemis ) == n_train_nodes )
+
+				# run the actual train / test
+				[ lvl_acc, lvl_wgt ] = _run_level( level_dir,
+				                                   train_data,
+				                                   train_data_info,
+				                                   test_data,
+				                                   test_data_info,
+				                                   log_path
+				                                 )
+
+				acc[ i_fold, i_level ] = lvl_acc
+
+				node_and_wgt_info = np.column_stack( ( i_fold_nodes,
+				                                       i_fold_hemis,
+				                                       lvl_wgt
+				                                     )
+				                                   )
+
+				node_and_wgt_file = os.path.join( level_dir,
+				                                  "%s_%s_%s_fold_%02d.txt" % (
+				                                    paths[ "mvpa" ]["node_wgt_base" ],
+				                                    roi_name,
+				                                    parc_lbl,
+				                                    ( i_fold + 1 )
+				                                  )
+				                                )
+
+				np.savetxt( node_and_wgt_file, node_and_wgt_info )
+
+		acc_file = os.path.join( roi_parc_dir,
+		                         "%s_%s_%s.txt" % ( paths[ "mvpa" ][ "acc_base" ],
+		                                            roi_name,
+		                                            parc_lbl
+		                                          )
+		                       )
+
+		np.savetxt( acc_file, acc )
 
 
 def _run_level( level_dir,

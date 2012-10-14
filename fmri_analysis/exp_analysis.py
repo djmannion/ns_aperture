ca8b2dd11abf306a152ab17552e7d099354a666c
Damien Mannion <d.mannion@unsw.edu.au>
2012-10-14 18:16:18 -0500

    -

--- fmri_analysis/exp_analysis.py
+++ fmri_analysis/exp_analysis.py
@@ -426,3 +426,37 @@ def roi_mean( paths, conf ):
 		            parc_mean
 		          )
 
+
+def group_agg( grp_paths, conf ):
+	"""Aggregate the univariate analyses across subjects"""
+
+	n_subj = len( conf[ "all_subj" ] )
+
+	n_rois = len( conf[ "ana" ][ "rois" ] ) * len( conf[ "ana" ][ "i_parc" ] )
+
+	uni_data = np.empty( n_subj, n_rois )
+	uni_data.fill( np.NAN )
+
+	for ( i_subj, subj_id ) in conf[ "all_subj" ]:
+
+		subj_conf = ns_aperture.config.get_conf( subj_id )
+		subj_paths = ns_aperture.fmri_analysis.paths.get_subj_paths( subj_conf )
+
+		i_roi = 0
+
+		for ( roi_name, _ ) in conf[ "ana" ][ "rois" ]:
+
+			roi_psc = np.loadtxt( paths[ "rois" ][ "parc_psc" ], roi_name )
+
+			uni_data[ i_subj, i_roi:( i_roi + 2 ) ] = roi_psc
+
+			i_roi += 2
+
+	np.savetxt( grp_paths[ "uni" ], uni_data )
+
+
+
+
+
+
+
